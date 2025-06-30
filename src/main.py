54737f68d0d20bdd0e832e30c231dae5508bf3b7
Zemphix <vafinaaa51@gmail.com>
2025-06-30 19:03:42 +0500

    minor changes

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ import uvicorn
 from fastapi.staticfiles import StaticFiles
 
 from api_v1.discounts.views import router as discounts_router
-from api_v1.categories.views import router as categories_router
 from utils.templates import templates
 from database.models import Base
 from database.core import async_engine
@@ -18,7 +17,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(discounts_router)
-app.include_router(categories_router)
+
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 
@@ -28,4 +27,4 @@ def index(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, port=8000, host='0.0.0.0')
+    uvicorn.run("main:app", reload=True, port=8000, host="0.0.0.0")
