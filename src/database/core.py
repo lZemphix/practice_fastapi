43f94dc433a5d9b4e0f9eba29761abe930557cfa
Zemphix <vafinaaa51@gmail.com>
2025-06-30 19:10:26 +0500

    minor changes

--- src/database/core.py
+++ src/database/core.py
@@ -1,6 +1,6 @@
 from config.config import settings
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy.orm import DeclarativeBase
 
 async_engine = create_async_engine(url=settings.db_url, echo=False)
 async_session: AsyncSession = async_sessionmaker(
@@ -10,5 +10,3 @@ async_session: AsyncSession = async_sessionmaker(
 
 class Base(DeclarativeBase):
     __abstract__ = True
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
