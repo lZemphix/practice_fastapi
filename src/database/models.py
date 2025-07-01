2036e9e3c188796306255e610504854e93677c26
Zemphix <vafinaaa51@gmail.com>
2025-07-01 23:14:27 +0500

    image column type changed to ImageType from fastapi_storage

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from .core import Base
+from fastapi_storages.integrations.sqlalchemy import ImageType
+from utils.storage import get_storage
 
 
 class Discount(Base):
@@ -13,7 +15,7 @@ class Discount(Base):
     promocode: Mapped[str] = mapped_column(nullable=True, default=None)
     validity_period: Mapped[str] = mapped_column()
     partner: Mapped[str] = mapped_column()
-    image: Mapped[str] = mapped_column()
+    image: Mapped[str] = mapped_column(ImageType(storage=get_storage()))
 
 
 class User(Base):
